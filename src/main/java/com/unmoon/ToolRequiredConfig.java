217a343a070b75bda9cebbaae3c29623a0b553c5
JZomDev <jzomerlei@gmail.com>
2025-10-29 00:01:44 -0400

    Make it a clickable alert

--- src/main/java/com/unmoon/ToolRequiredConfig.java
+++ src/main/java/com/unmoon/ToolRequiredConfig.java
@@ -1,46 +1,35 @@
 package com.unmoon;
 
+import static com.unmoon.ToolRequiredConfig.CONFIG_GROUP;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("tool-required")
+@ConfigGroup(CONFIG_GROUP)
 public interface ToolRequiredConfig extends Config
 {
-        @ConfigItem(
-                keyName = "chopDown",
-                name = "Remove Chop down",
-                description = "Remove Chop down option from trees if you have no axe."
-        )
-        default boolean chopDown() {return true;}
+	String CONFIG_GROUP = "tool-required";
+	String LAST_SEEN_VERSION = "0.0";
 
-        @ConfigItem(
-                keyName = "mine",
-                name = "Remove Mine",
-                description = "Remove Mine option from rocks if you have no pickaxe."
-        )
-        default boolean mine() {return true;}
+	@ConfigItem(
+		keyName = "chopDown",
+		name = "Remove Chop down",
+		description = "Remove Chop down option from trees if you have no axe."
+	)
+	default boolean chopDown() {return true;}
 
-        @ConfigItem(
-                keyName = "farming",
-                name = "Remove Farming",
-                description = "Remove Pick and Harvest option from relevant patches if you have no magic secateurs."
-        )
-        default boolean farm() {return true;}
+	@ConfigItem(
+		keyName = "mine",
+		name = "Remove Mine",
+		description = "Remove Mine option from rocks if you have no pickaxe."
+	)
+	default boolean mine() {return true;}
 
-        @ConfigItem(
-                keyName = "acknowledgeUpdate",
-                name = "Acknowledge Update",
-                description = "Check this box to acknowledge you have seen the latest plugin updates."
-        )
-        default boolean acknowledgeUpdate() {return false;}
-
-        @ConfigItem(
-                keyName = "lastVersionSeen",
-                name = "Last Version Seen",
-                description = "The last plugin version login message the user acknowledged.",
-                hidden = true
-        )
-        default String lastVersionSeen() {return "";}
+	@ConfigItem(
+		keyName = "farming",
+		name = "Remove Farming",
+		description = "Remove Pick and Harvest option from relevant patches if you have no magic secateurs."
+	)
+	default boolean farm() {return true;}
 
 }
