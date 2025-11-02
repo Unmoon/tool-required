f546cb7777f19b6ba35b223d75a219a4df7ad408
Alex Bristow CTR <alex.bristow.ctr@kesselrun.org>
2025-11-02 14:49:57 -0500

    formatting

--- src/main/java/com/unmoon/ToolRequiredConfig.java
+++ src/main/java/com/unmoon/ToolRequiredConfig.java
@@ -9,19 +9,19 @@ import net.runelite.client.config.ConfigItem;
 public interface ToolRequiredConfig extends Config
 {
 	String CONFIG_GROUP = "tool-required";
-        String LAST_VERSION_SEEN = "0.0.0";
+    String LAST_VERSION_SEEN = "0.0.0";
 
 	@ConfigItem(
-		keyName = "chopDown",
-		name = "Remove Chop down",
-		description = "Remove Chop down option from trees if you have no axe."
+	    keyName = "chopDown",
+	    name = "Remove Chop down",
+	    description = "Remove Chop down option from trees if you have no axe."
 	)
 	default boolean chopDown() {return true;}
 
 	@ConfigItem(
-		keyName = "mine",
-		name = "Remove Mine",
-		description = "Remove Mine option from rocks if you have no pickaxe."
+	    keyName = "mine",
+	    name = "Remove Mine",
+	    description = "Remove Mine option from rocks if you have no pickaxe."
 	)
 	default boolean mine() {return true;}
 
